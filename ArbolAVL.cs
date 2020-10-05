c934768950f2cc8960fe5b37fd8ced4b1e8131a2
NicoDVK <71187149+NicoDVKa@users.noreply.github.com>
2020-10-05 14:24:40 -0300

    Agrego los recorridos y el método incluye().
    El método incluye() no funciona correctamente, ya que, no se cumple la propiedad del orden

--- ArbolAVL.cs
+++ ArbolAVL.cs
@@ -62,7 +62,93 @@ namespace AVL
 		public void rotacionDobleIzquierda() {
 		
 		}
-		
-		
+
+		//Metodos del Arbol Binario de Busqueda
+		//Metodo Incluye() del ABB
+		public bool incluye(IComparable elemento)
+		{
+			bool verificar = false;
+
+			if (this.dato == null)
+			{
+				return false;
+			}
+			else
+			{
+				if (int.Parse(elemento.ToString()) == int.Parse(this.dato.ToString()))
+				{
+					return true;
+				}
+				else
+				{
+					if (int.Parse(elemento.ToString()) > int.Parse(this.dato.ToString()))
+					{
+						if (this.getHijoDerecho() == null) return false;
+						verificar = this.getHijoDerecho().incluye(elemento);
+
+					}
+					else
+					{
+						if (this.getHijoIzquierdo() == null) return false;
+						verificar = this.getHijoIzquierdo().incluye(elemento);
+
+					}
+				}
+			}
+
+			return verificar;
+		}
+
+		//Recorridos. son los mismos que en los Arboles Binarios de Busqueda
+		public void preorden()
+		{
+			//Primero la raiz y luego los hijos izquierdos y derechos
+
+			Console.Write(this.getDatoRaiz() + " ");
+
+			if (this.getHijoIzquierdo() != null)
+			{
+				this.getHijoIzquierdo().preorden();
+			}
+
+			if (this.getHijoDerecho() != null)
+			{
+				this.getHijoDerecho().preorden();
+			}
+		}
+
+		public void inorden()
+		{
+			//Primero el hijo izquierdo luego la raiz y por ultimo el hijo derecho
+			if (this.getHijoIzquierdo() != null)
+			{
+				this.getHijoIzquierdo().inorden();
+			}
+
+			Console.Write(this.getDatoRaiz() + " ");
+
+			if (this.getHijoDerecho() != null)
+			{
+				this.getHijoDerecho().inorden();
+			}
+		}
+
+
+		public void postorden()
+		{
+			//Primero los hijos, izquierdo y derecho, y luego la raiz
+			if (getHijoIzquierdo() != null)
+			{
+				this.getHijoIzquierdo().postorden();
+			}
+
+			if (this.getHijoDerecho() != null)
+			{
+				this.getHijoDerecho().postorden();
+			}
+
+			Console.Write(this.getDatoRaiz() + " ");
+		}
+
 	}
 }
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AVL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolAVL arbolAVL = new ArbolAVL(1);
+            ArbolAVL hijoIzquierdo = new ArbolAVL(2);
+            ArbolAVL hijoDerecho = new ArbolAVL(3);
+            ArbolAVL hijoDelIzquierdo = new ArbolAVL(4);
+            
+            arbolAVL.agregarHijoIzquierdo(hijoIzquierdo);
+            arbolAVL.agregarHijoDerecho(hijoDerecho);
+
+            hijoIzquierdo.agregarHijoIzquierdo(hijoDelIzquierdo);
+
+            hijoDelIzquierdo.agregarHijoIzquierdo(new ArbolAVL(5));
+            hijoDelIzquierdo.agregarHijoDerecho(new ArbolAVL(6));
+
+            //Todavia no se cumplen las propiedades de los arboles AVL.
+
+            //Comprobacion de los metodos de los ABB
+
+            Console.Write("Preorden: ");
+            arbolAVL.preorden();//Funciona
+            Console.WriteLine();
+            Console.Write("Inorden: ");
+            arbolAVL.inorden();//Funciona
+            Console.WriteLine();
+            Console.Write("Postorden: ");
+            arbolAVL.postorden();//Funciona
+            Console.WriteLine();
+            
+            
+            //Incluye() todavia no funciona correctamente ya que no se cumple la propiedad de orden
+            int dato = 2;
+            Console.WriteLine("El dato {0} esta en el ab? {1}", dato, arbolAVL.incluye(dato));
+
+
+            Console.WriteLine("Presione un tecla para salir");
+            Console.ReadKey(true);
+        }
+    }
+}
